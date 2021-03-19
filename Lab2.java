15c64ec8e622027b2f90b782a00d4a48c842dd89
EnterToExit <78351927+EnterToExit@users.noreply.github.com>
2021-03-19 18:22:27 +0300

    change lab2 task5

--- Lab2.java
+++ Lab2.java
@@ -37,7 +37,7 @@ public class Lab2 {
         if (a == b && a == c) {
             return 3;
         }
-        if (a == b || a == c) {
+        if (a == b || a == c || b == c) {
             return 2;
         }
         return 0;
