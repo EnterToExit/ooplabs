b63fe37a86e89e66cb4ebeab221b51888281ba38
EnterToExit <78351927+EnterToExit@users.noreply.github.com>
2021-03-19 17:55:01 +0300

    labs 19 march

--- Lab2.java
+++ Lab2.java
@@ -1,4 +1,93 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Lab2 {
+    //1
+    public int oppositeHouse(int num, int lenght) {
+        return (lenght * 2) - num + 1;
+    }
+
+    //2
+    public String nameShuffle(String name) {
+        String[] namesSplit = name.split(" ");
+        return namesSplit[1] + " " + namesSplit[0];
+    }
+
+    //3
+    public double discount(double cost, int dis) {
+        return cost -((cost / 100) * dis);
+    }
+
+    //4
+    public int differenceMaxMin(String args) {
+        String[] args_splitted = args.split(";");
+        int[] intarray = new int[args_splitted.length];
+        for (int i = 0; i < args_splitted.length; i++){
+            intarray[i]=Integer.parseInt(args_splitted[i].trim());
+        }
+        Arrays.sort(intarray);
+        int min = intarray[0];
+        int max = intarray[intarray.length - 1];
+        return max - min;
+    }
+
+    //5
+    public int equal(int a, int b, int c) {
+        if (a == b && a == c) {
+            return 3;
+        }
+        if (a == b || a == c) {
+            return 2;
+        }
+        return 0;
+    }
+
+    //6
+    public String reverse(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    //7
+    public int programmers(int first, int second, int third) {
+        int[] array = {first, second, third};
+        Arrays.sort(array);
+        int min = array[0];
+        int max = array[array.length - 1];
+        return max - min;
+    }
+
+    //8
+    public boolean getXO(String str) {
+        int countX = 0;
+        int countO = 0;
+        String lower_str = str.toLowerCase();
+        for (int i = 0; i < str.length(); i++) {
+            if (lower_str.charAt(i) == 'x') {
+                countX += 1;
+            }
+            if (lower_str.charAt(i) == 'o') {
+                countO += 1;
+            }
+        }
+        return countX == countO;
+    }
+
+    //9
+    public String bomb(String str) {
+        return str.toLowerCase().contains("bomb") ? "DUCK!" : "Relax, there's no bomb.";
+    }
+
+    //10
+    public boolean sameAscii(String arg1, String arg2) {
+        int asciiSum1 = 0;
+        int asciiSum2 = 0;
+        for (int i = 0; i < arg1.length(); i++) {
+            asciiSum1 += (int) arg1.charAt(i);
+        }
+        for (int i = 0; i < arg2.length(); i++) {
+            asciiSum2 += (int) arg2.charAt(i);
+        }
+        return asciiSum1 == asciiSum2;
+    }
 }
--- Main.java
+++ Main.java
@@ -12,7 +12,7 @@ public class Main {
         try {
             switch (main_choice) {
                 case 1:
-                    Lab1 lab = new Lab1();
+                    Lab1 lab1 = new Lab1();
                     while (choice != 0) {
                         try {
                             System.out.print("Please pick a number to test function > ");
@@ -21,34 +21,34 @@ public class Main {
                                 System.out.println("Please enter arguments");
                             switch (choice) {
                                 case 1:
-                                    System.out.println(lab.convert(scanner.nextInt()));
+                                    System.out.println(lab1.convert(scanner.nextInt()));
                                     break;
                                 case 2:
-                                    System.out.println(lab.points(scanner.nextInt(), scanner.nextInt()));
+                                    System.out.println(lab1.points(scanner.nextInt(), scanner.nextInt()));
                                     break;
                                 case 3:
-                                    System.out.println(lab.footballPoints(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()));
+                                    System.out.println(lab1.footballPoints(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()));
                                     break;
                                 case 4:
-                                    System.out.println(lab.divisibleByFive(scanner.nextInt()));
+                                    System.out.println(lab1.divisibleByFive(scanner.nextInt()));
                                     break;
                                 case 5:
-                                    System.out.println(lab.and(scanner.nextBoolean(), scanner.nextBoolean()));
+                                    System.out.println(lab1.and(scanner.nextBoolean(), scanner.nextBoolean()));
                                     break;
                                 case 6:
-                                    System.out.println(lab.howManyWalls(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()));
+                                    System.out.println(lab1.howManyWalls(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()));
                                     break;
                                 case 7:
-                                    System.out.println(lab.squared(scanner.nextInt()));
+                                    System.out.println(lab1.squared(scanner.nextInt()));
                                     break;
                                 case 8:
-                                    System.out.println(lab.profitableGamble(scanner.nextFloat(), scanner.nextFloat(), scanner.nextFloat()));
+                                    System.out.println(lab1.profitableGamble(scanner.nextFloat(), scanner.nextFloat(), scanner.nextFloat()));
                                     break;
                                 case 9:
-                                    System.out.println(lab.frames(scanner.nextInt(), scanner.nextInt()));
+                                    System.out.println(lab1.frames(scanner.nextInt(), scanner.nextInt()));
                                     break;
                                 case 10:
-                                    System.out.println(lab.mod(scanner.nextInt(), scanner.nextInt()));
+                                    System.out.println(lab1.mod(scanner.nextInt(), scanner.nextInt()));
                                     break;
                                 case 0:
                                     System.out.println("Goodbye!");
@@ -62,7 +62,56 @@ public class Main {
                         }
                     }
                     break;
-
+                case 2:
+                    Lab2 lab2 = new Lab2();
+                    while (choice != 0) {
+                        try {
+                            System.out.print("Please pick a number to test function > ");
+                            choice = scanner.nextInt();
+                            if (choice >= 1 && choice <= 10)
+                                System.out.println("Please enter arguments");
+                            switch (choice) {
+                                case 1:
+                                    System.out.println(lab2.oppositeHouse(scanner.nextInt(), scanner.nextInt()));
+                                    break;
+                                case 2:
+                                    System.out.println(lab2.nameShuffle("Donald Trump"));
+                                    break;
+                                case 3:
+                                    System.out.println(lab2.discount(scanner.nextDouble(), scanner.nextInt()));
+                                    break;
+                                case 4:
+                                    System.out.println(lab2.differenceMaxMin(scanner.next()));
+                                    break;
+                                case 5:
+                                    System.out.println(lab2.equal(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()));
+                                    break;
+                                case 6:
+                                    System.out.println(lab2.reverse(scanner.next()));
+                                    break;
+                                case 7:
+                                    System.out.println(lab2.programmers(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()));
+                                    break;
+                                case 8:
+                                    System.out.println(lab2.getXO(scanner.next()));
+                                    break;
+                                case 9:
+                                    System.out.println(lab2.bomb("There bomb!"));
+                                    break;
+                                case 10:
+                                    System.out.println(lab2.sameAscii(scanner.next(), scanner.next()));
+                                    break;
+                                case 0:
+                                    System.out.println("Goodbye!");
+                                    break;
+                                default:
+                                    System.out.println("No such function");
+                            }
+                        } catch (Exception e) {
+                            System.out.println("invalid argument");
+                            scanner.next();
+                        }
+                    }
             }
         } catch (Exception e) {
             System.out.println("invalid argument");
