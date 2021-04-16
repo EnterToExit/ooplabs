50f5ad9ebea961f2ab76f171479a842a42569d76
EnterToExit <78351927+EnterToExit@users.noreply.github.com>
2021-04-16 16:32:36 +0300

    prak3

--- Main.java
+++ Main.java
@@ -12,7 +12,7 @@ public class Main {
         try {
             switch (main_choice) {
                 case 1:
-                    Lab1 lab1 = new Lab1();
+                    Prak1 prak1 = new Prak1();
                     while (choice != 0) {
                         try {
                             System.out.print("Please pick a number to test function > ");
@@ -21,34 +21,34 @@ public class Main {
                                 System.out.println("Please enter arguments");
                             switch (choice) {
                                 case 1:
-                                    System.out.println(lab1.convert(scanner.nextInt()));
+                                    System.out.println(prak1.convert(scanner.nextInt()));
                                     break;
                                 case 2:
-                                    System.out.println(lab1.points(scanner.nextInt(), scanner.nextInt()));
+                                    System.out.println(prak1.points(scanner.nextInt(), scanner.nextInt()));
                                     break;
                                 case 3:
-                                    System.out.println(lab1.footballPoints(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()));
+                                    System.out.println(prak1.footballPoints(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()));
                                     break;
                                 case 4:
-                                    System.out.println(lab1.divisibleByFive(scanner.nextInt()));
+                                    System.out.println(prak1.divisibleByFive(scanner.nextInt()));
                                     break;
                                 case 5:
-                                    System.out.println(lab1.and(scanner.nextBoolean(), scanner.nextBoolean()));
+                                    System.out.println(prak1.and(scanner.nextBoolean(), scanner.nextBoolean()));
                                     break;
                                 case 6:
-                                    System.out.println(lab1.howManyWalls(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()));
+                                    System.out.println(prak1.howManyWalls(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()));
                                     break;
                                 case 7:
-                                    System.out.println(lab1.squared(scanner.nextInt()));
+                                    System.out.println(prak1.squared(scanner.nextInt()));
                                     break;
                                 case 8:
-                                    System.out.println(lab1.profitableGamble(scanner.nextFloat(), scanner.nextFloat(), scanner.nextFloat()));
+                                    System.out.println(prak1.profitableGamble(scanner.nextFloat(), scanner.nextFloat(), scanner.nextFloat()));
                                     break;
                                 case 9:
-                                    System.out.println(lab1.frames(scanner.nextInt(), scanner.nextInt()));
+                                    System.out.println(prak1.frames(scanner.nextInt(), scanner.nextInt()));
                                     break;
                                 case 10:
-                                    System.out.println(lab1.mod(scanner.nextInt(), scanner.nextInt()));
+                                    System.out.println(prak1.mod(scanner.nextInt(), scanner.nextInt()));
                                     break;
                                 case 0:
                                     System.out.println("Goodbye!");
@@ -63,7 +63,7 @@ public class Main {
                     }
                     break;
                 case 2:
-                    Lab2 lab2 = new Lab2();
+                    Prak2 prak2 = new Prak2();
                     while (choice != 0) {
                         try {
                             System.out.print("Please pick a number to test function > ");
@@ -72,34 +72,34 @@ public class Main {
                                 System.out.println("Please enter arguments");
                             switch (choice) {
                                 case 1:
-                                    System.out.println(lab2.oppositeHouse(scanner.nextInt(), scanner.nextInt()));
+                                    System.out.println(prak2.oppositeHouse(scanner.nextInt(), scanner.nextInt()));
                                     break;
                                 case 2:
-                                    System.out.println(lab2.nameShuffle("Donald Trump"));
+                                    System.out.println(prak2.nameShuffle("Donald Trump"));
                                     break;
                                 case 3:
-                                    System.out.println(lab2.discount(scanner.nextDouble(), scanner.nextInt()));
+                                    System.out.println(prak2.discount(scanner.nextDouble(), scanner.nextInt()));
                                     break;
                                 case 4:
-                                    System.out.println(lab2.differenceMaxMin(scanner.next()));
+                                    System.out.println(prak2.differenceMaxMin(scanner.next()));
                                     break;
                                 case 5:
-                                    System.out.println(lab2.equal(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()));
+                                    System.out.println(prak2.equal(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()));
                                     break;
                                 case 6:
-                                    System.out.println(lab2.reverse(scanner.next()));
+                                    System.out.println(prak2.reverse(scanner.next()));
                                     break;
                                 case 7:
-                                    System.out.println(lab2.programmers(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()));
+                                    System.out.println(prak2.programmers(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()));
                                     break;
                                 case 8:
-                                    System.out.println(lab2.getXO(scanner.next()));
+                                    System.out.println(prak2.getXO(scanner.next()));
                                     break;
                                 case 9:
-                                    System.out.println(lab2.bomb("There bomb!"));
+                                    System.out.println(prak2.bomb("There bomb!"));
                                     break;
                                 case 10:
-                                    System.out.println(lab2.sameAscii(scanner.next(), scanner.next()));
+                                    System.out.println(prak2.sameAscii(scanner.next(), scanner.next()));
                                     break;
                                 case 0:
                                     System.out.println("Goodbye!");
@@ -109,6 +109,63 @@ public class Main {
                             }
                         } catch (Exception e) {
                             System.out.println("invalid argument");
+                            scanner.next();
+                        }
+                    }
+                case 3:
+                    Prak3 prak3 = new Prak3();
+                    while (choice != 0) {
+                        try {
+                            System.out.print("Please pick a number to test function > ");
+                            choice = scanner.nextInt();
+                            if (choice >= 1 && choice <= 10)
+                            switch (choice) {
+                                case 1:
+                                    prak3.millionsRounding();
+                                    break;
+                                case 2:
+                                    System.out.println(prak3.otherSides(3.0));
+                                    break;
+                                case 9:
+                                    double[] array = {1.0,2.0,3.0,4.0,5.0,6.0};
+                                    System.out.println(prak3.mean(array));
+                                    break;
+                                case 3:
+                                    System.out.println(prak3.rps("scissor","rock"));
+                                    System.out.println(prak3.rps("scissor","scissor"));
+                                    System.out.println(prak3.rps("paper","rock"));
+                                    break;
+                                case 4:
+                                    int[] input = {1, 2, 3, 4, 5, 6, 8, 9};
+                                    System.out.println(prak3.warOfNumbers(input));
+                                    break;
+                                case 5:
+                                    System.out.println(prak3.reverseCase("Lorem ipsum"));
+                                    break;
+                                case 6:
+                                    System.out.println(prak3.inatorInator(scanner.next()));
+                                    break;
+                                case 7:
+                                    System.out.println(prak3.doesBrickFit(500, 500, 500, 1, 2));
+                                    break;
+                                case 8:
+                                    System.out.println(prak3.totalDistance(36.1, 8.6, 3, true));
+                                    System.out.println(prak3.totalDistance(55.5, 5.5, 5, false));
+                                    System.out.println(prak3.totalDistance(70.0, 7.0, 0, false));
+                                    break;
+                                case 10:
+                                    System.out.println(prak3.parityAnalysis(243));
+                                    System.out.println(prak3.parityAnalysis(12));
+                                    System.out.println(prak3.parityAnalysis(3));
+                                    break;
+                                case 0:
+                                    System.out.println("Goodbye!");
+                                    break;
+                                default:
+                                    System.out.println("No such function");
+                            }
+                        } catch (Exception e) {
+                            System.out.println(e);
                             scanner.next();
                         }
                     }
---
+++ Prak3.java
@@ -0,0 +1,150 @@
+package com.company;
+
+import sun.security.util.ArrayUtil;
+
+import java.text.DecimalFormat;
+import java.util.*;
+
+
+public class Prak3 {
+
+    private Double roundTo(Double toRound, int n) {
+        return (double) Math.round(toRound / Math.pow(10, n)) * Math.pow(10, n);
+    }
+
+
+    //1
+    public void millionsRounding() {
+        Map<String, Integer> places = new HashMap<>();
+        places.put("Nice", 942208);
+        places.put("Abu Dhabi", 1482816);
+        places.put("Naples", 2186853);
+        places.put("Vatican City", 572);
+        printValues(places);
+    }
+
+    public void printValues(Map<String, Integer> map) {
+        for (Map.Entry<String, Integer> pair : map.entrySet()) {
+            double value = pair.getValue();
+            String key = pair.getKey();
+            value = Math.round(value / 1000000) * 1000000;
+            int x = (int) value;
+            System.out.println("[" + key + " " + x + "]");
+        }
+    }
+
+    //2
+    public ArrayList<Double> otherSides(Double len) {
+        return new ArrayList<Double>(Arrays.asList(roundTo(2 * len, -2), roundTo(Math.sqrt(3) * len, -2)));
+    }
+
+    //3
+    public String rps(String arg1, String arg2) {
+        Map<String, Integer> states = new HashMap<>();
+        states.put("rock", 1);
+        states.put("scissor", 2);
+        states.put("paper", 3);
+        int player1 = states.get(arg1);
+        int player2 = states.get(arg2);
+        int result = Math.abs(player1 - player2);
+        String gameResult = "Draw";
+        switch (result) {
+            case 2:
+                gameResult = "Player 1 wins";
+                break;
+            case 1:
+                gameResult = "Player 2 wins";
+                break;
+        }
+        return gameResult;
+    }
+
+    //4
+    public int warOfNumbers(int[] array) {
+        int even = 0;
+        int odd = 0;
+        for (int i : array) {
+            if (i % 2 == 0) {
+                even += i;
+                continue;
+            }
+            odd += i;
+        }
+        return Math.abs(even - odd);
+    }
+
+    //5
+    public String reverseCase(String text) {
+        char[] chars = text.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (Character.isUpperCase(c)) {
+                chars[i] = Character.toLowerCase(c);
+            } else if (Character.isLowerCase(c)) {
+                chars[i] = Character.toUpperCase(c);
+            }
+        }
+        return new String(chars);
+    }
+
+    //6
+    public String inatorInator(String arg) {
+        String vowels = "aeiouy";
+        String lastChar = arg.substring(arg.length() - 1);
+        String output = "";
+        if (vowels.contains(lastChar)) {
+            output = arg + "-inator" + " " + arg.length() + "000";
+        } else {
+            output = arg + "inator" + " " + arg.length() + "000";
+        }
+        return output;
+    }
+
+    //7
+    public boolean doesBrickFit(int height, int width, int thickness, int w, int h) {
+        ArrayList<Integer> brickDimensions = new ArrayList<Integer>(Arrays.asList(height, width, thickness));
+        brickDimensions.remove(Collections.max(brickDimensions));
+        return (brickDimensions.get(0) <= w && brickDimensions.get(1) <= h) || (brickDimensions.get(1) <= w && brickDimensions.get(0) <= h);
+    }
+
+    //8
+    public double totalDistance(double petrol, double petrolC, int passenger, boolean aC) {
+        double distance;
+        double petrolUsage;
+        if (aC) {
+            petrolUsage = petrolC * (1 + 0.05 * passenger) * (1 + 0.1);
+        } else {
+            petrolUsage = petrolC * (1 + 0.05 * passenger);
+        }
+        distance = petrol / petrolUsage * 100;
+        return distance;
+    }
+
+    //9
+    public double mean(double[] args) {
+        double sum = 0;
+        for (double i : args) {
+            sum += i;
+        }
+        return sum / args.length;
+    }
+
+    //10
+    public boolean parityAnalysisOld(String argStr) {
+        int arg = Integer.parseInt(argStr);
+        int argSum = 0;
+        String[] argSplit = argStr.split("");
+        for (int i = 0; i < argStr.length(); i++) {
+            argSum += Integer.parseInt(argSplit[i]);
+        }
+        return (arg % 2 - argSum % 2) == 0;
+    }
+
+    public boolean parityAnalysis(int arg) {
+        int argSum = 0;
+        for (int i = arg; i > 0; i /= 10) {
+            argSum += i % 10;
+        }
+        return (arg % 2 - argSum % 2) == 0;
+    }
+}
